d7d67499e6b8f8c350f08169c1056b8d0ee67ed5
Quanwen Zhao <guestart@163.com>
2018-05-09 15:57:32 +0800

    Update ash_event_count_topN_new.sql

--- ash_event_count_topN_new.sql
+++ ash_event_count_topN_new.sql
@@ -1,16 +1,17 @@
-rem
-rem     Script:        ash_event_count_topN_new.sql
-rem     Author:        Quanwen Zhao
-rem     Dated:         Apr 25, 2018
-rem
-rem     Purpose:  
-rem     This sql script usually statistics Top-N event counts,
-rem     and when it runs you can only input 3 parameters - start_time you want to appoint,
-rem     end_time and num that is Top ROWNUM.
-rem
-rem     Modified:      May 08, 2018 - adding the justify center to the column "session_state", "blocking_session" and "blocking_session_serial#" 
-rem                                   for running nicely; at the same time, also adding some interactive and friendly prompts when inputing 3 
-rem                                   parameters.
+REM
+REM     Script:        ash_event_count_topN_new.sql
+REM     Author:        Quanwen Zhao
+REM     Dated:         Apr 25, 2018
+REM
+REM     Purpose:  
+REM       This sql script usually statistics Top-N event counts,
+REM       and when it runs you can only input 3 parameters - start_time you want to appoint,
+REM       end_time and num that is Top ROWNUM.
+REM
+REM     Modified:      May 08, 2018 - adding the justify center to the column "session_state", "blocking_session" and "blocking_session_serial#" 
+REM                                   for running nicely; at the same time, also adding some interactive and friendly prompts when inputing 3 
+REM                                   parameters.
+REM
 
 SET LINESIZE 400
 SET PAGESIZE 300
@@ -23,15 +24,15 @@ COLUMN  session_state             HEADING  "session|state"             FORMAT  a
 COLUMN  blocking_session          HEADING  "blocking|session"          FORMAT  99999999  JUSTIFY  center
 COLUMN  blocking_session_serial#  HEADING  "blocking|session|serial#"  FORMAT  99999999  JUSTIFY  center
 
-prompt Enter begin time ([YYYY-MM-DD HH24:MI:SS]):
-prompt Examples: 2018-03-13 11:20:00
-prompt 
-prompt Enter end time ([YYYY-MM-DD HH24:MI:SS]):
-prompt Examples: 2018-03-13 11:30:00
-prompt 
-prompt Enter nums of Top-N ROWNUM:
-prompt Examples: 15, 25 or 35 ...
-prompt 
+PROMPT Enter begin time ([YYYY-MM-DD HH24:MI:SS]):
+PROMPT Examples: 2018-03-13 11:20:00
+PROMPT 
+PROMPT Enter end time ([YYYY-MM-DD HH24:MI:SS]):
+PROMPT Examples: 2018-03-13 11:30:00
+PROMPT 
+PROMPT Enter nums of Top-N ROWNUM:
+PROMPT Examples: 15, 25 or 35 ...
+PROMPT 
 
 SELECT *
 FROM
