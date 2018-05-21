279f2bf55be940b6ddcfd9c6cf991a88deb6cf50
Quanwen Zhao <guestart@163.com>
2018-05-21 17:20:51 +0800

    Update all_tables_stats_on_all_proc_users.sql

--- all_tables_stats_on_all_proc_users.sql
+++ all_tables_stats_on_all_proc_users.sql
@@ -27,50 +27,16 @@ SELECT owner
        , last_analyzed
        , stale_stats
 FROM dba_tab_statistics
--- WHERE owner NOT IN (
---                    'ANONYMOUS'
---                    , 'APEX_030200'
---                    , 'APEX_PUBLIC_USER'
---                    , 'APPQOSSYS'
---                    , 'CTXSYS'
---                    , 'DBSNMP'
---                    , 'DIP'
---                    , 'EXFSYS'
---                    , 'FLOWS_FILES'
---                    , 'MDDATA'
---                    , 'MDSYS'
---                    , 'MGMT_VIEW'
---                    , 'OLAPSYS'
---                    , 'ORACLE_OCM'
---                    , 'ORDDATA'
---                    , 'ORDPLUGINS'
---                    , 'ORDSYS'
---                    , 'OUTLN'
---                    , 'OWBSYS'
---                    , 'OWBSYS_AUDIT'
---                    , 'SCOTT'
---                    , 'SI_INFORMTN_SCHEMA'
---                    , 'SPATIAL_CSW_ADMIN_USR'
---                    , 'SPATIAL_WFS_ADMIN_USR'
---                    , 'SQLTXADMIN'
---                    , 'SQLTXPLAIN'
---                    , 'SYS'
---                    , 'SYSMAN'
---                    , 'SYSTEM'
---                    , 'WMSYS'
---                    , 'XDB'
---                    , 'XS$NULL'
---                    )
 WHERE owner NOT IN (
-                      SELECT name schema_to_exclude
-                      FROM system.LOGSTDBY$SKIP_SUPPORT
-                      WHERE action = 0
-                      ORDER BY schema_to_exclude
-                      ;
+                     SELECT name schema_to_exclude
+                     FROM system.LOGSTDBY$SKIP_SUPPORT
+                     WHERE action = 0
+                     ORDER BY schema_to_exclude
+                     ;
                    )
 AND owner NOT IN (
-                      'SQLTXADMIN'
-                      , 'SQLTXPLAIN'
+                   'SQLTXADMIN'
+                   , 'SQLTXPLAIN'
                  )
 AND table_name NOT LIKE 'BIN$%'
 ORDER BY owner
