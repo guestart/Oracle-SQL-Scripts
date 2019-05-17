15a6ab7acde7192e12b06d00f7807f40bd386be5
Quanwen Zhao <guestart@163.com>
2019-05-17 16:42:25 +0800

    Modifying the keyword of sql with lowercase to uppercase.

--- shared_memory_rank_top_5_sql_on_sqlstats.sql
+++ shared_memory_rank_top_5_sql_on_sqlstats.sql
@@ -1,20 +1,30 @@
 ===============================================================================================
+-- Script: shared_memory_rank_top_5_sql_on_sqlstats.sql
+-- Author: Quanwen Zhao
+-- Updated: May 17, 2019
 -- Ranking Top 5 SQL for shared memory (Memory hogs) on v$sqlstats
 -- Total shared memory (in bytes) currently occupied by all cursors with this SQL text and plan
 -- Trying not to check v$sql, as you can see Connor's this blog post
 -- https://connor-mcdonald.com/2019/03/04/less-slamming-vsql/
 ===============================================================================================
-set linesize 32767
-set pagesize 50000
-col sql_id for a13
-col sql_text for a60
-col sharable_mem for 999,999,999,999,999
-select *
-from (select sql_id, sql_text, sharable_mem,
-             dense_rank() over (order by sharable_mem desc) sharable_mem_rank
-      from v$sqlstats
-      where sharable_mem > 10000000
+
+SET LINESIZE 32767
+SET PAGESIZE 50000
+
+COLUMN sql_id FORMAT a13
+COLUMN sql_text FORMAT a60
+COLUMN sharable_mem FORMAT 999,999,999,999,999
+
+SELECT *
+FROM (SELECT sql_id
+             , sql_text
+             , sharable_mem
+             , DENSE_RANK() OVER (ORDER BY sharable_mem DESC) AS sharable_mem_rank
+      FROM v$sqlstats
+      WHERE sharable_mem > 10000000
      )
-where sharable_mem_rank <= 5;
-set linesize 80
-set pagesize 14
+WHERE sharable_mem_rank <= 5
+/
+
+SET LINESIZE 80
+SET PAGESIZE 14
