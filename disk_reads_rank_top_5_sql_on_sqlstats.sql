e873122a928de290cc2ac6828de358800349a7fc
Quanwen Zhao <guestart@163.com>
2019-05-17 16:30:25 +0800

    Modifying the keyword of SQL with lowercase to uppercase.

--- disk_reads_rank_top_5_sql_on_sqlstats.sql
+++ disk_reads_rank_top_5_sql_on_sqlstats.sql
@@ -1,20 +1,30 @@
 ====================================================================
+-- Script: disk_reads_rank_top_5_sql_on_sqlstats.sql
+-- Author: Quanwen Zhao
+-- Updated: May 17, 2019
 -- Ranking Top 5 SQL for disk_reads (High I/O) on v$sqlstats
 -- Number of disk reads for all cursors with this SQL text and plan
 -- Trying not to check v$sql, as you can see Connor's this blog post
 -- https://connor-mcdonald.com/2019/03/04/less-slamming-vsql/
 ====================================================================
-set linesize 32767
-set pagesize 50000
-col sql_id for a13
-col sql_text for a70
-col disk_reads for 999,999,999,999,999
-select *
-from (select sql_id, sql_text, disk_reads,
-      dense_rank() over (order by disk_reads desc) disk_reads_rank
-      from v$sql
-      where disk_reads > 100000
+
+SET LINESIZE 32767
+SET PAGESIZE 50000
+
+COLUMN sql_id FORMAT a13
+COLUMN sql_text FORMAT a70
+COLUMN disk_reads FORMAT 999,999,999,999,999
+
+SELECT *
+FROM (SELECT sql_id
+             , sql_text
+             , disk_reads
+             , DENSE_RANK() OVER (ORDER BY disk_reads DESC) AS disk_reads_rank
+      FROM v$sql
+      WHERE disk_reads > 100000
      )
-where disk_reads_rank <= 5;
-set linesize 80
-set pagesize 14
+WHERE disk_reads_rank <= 5
+/
+
+SET LINESIZE 80
+SET PAGESIZE 14
