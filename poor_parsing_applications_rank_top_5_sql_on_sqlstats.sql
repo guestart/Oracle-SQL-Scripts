6daad8b489fb3584d9ee5124c4f3061ff9c6ae5e
Quanwen Zhao <guestart@163.com>
2019-05-17 16:36:49 +0800

    Modifying the keyword of sql with lowercase to uppercase.

--- poor_parsing_applications_rank_top_5_sql_on_sqlstats.sql
+++ poor_parsing_applications_rank_top_5_sql_on_sqlstats.sql
@@ -1,22 +1,32 @@
 ==============================================================================================================
+-- Script: poor_parsing_applications_rank_top_5_sql_on_sqlstats.sql
+-- Author: Quanwen Zhao
+-- Updated: May 17, 2019
 -- Ranking Top 5 SQL for poor parsing applications (parse_calls/executions) on v$sqlstats
 -- parse_calls, Number of parse calls for all cursors with this SQL text and plan
 -- executions, Number of executions that took place on this object since it was brought into the library cache
 -- Trying not to check v$sql, as you can see Connor's this blog post
 -- https://connor-mcdonald.com/2019/03/04/less-slamming-vsql/
 ==============================================================================================================
-set linesize 32767
-set pagesize 50000
-col sql_id for a13
-col sql_text for a70
-col parse_calls/executions for 999,999,999,999,999 heading 'parse_calls|executions'
-col parse_app_rank for 999 heading 'parse_app|rank'
-select *
-from (select sql_id, sql_text, parse_calls/executions,
-      dense_rank() over (order by decode(executions,0,0,parse_calls/executions) desc) parse_app_rank
-      from v$sql
-      where decode(executions,0,0,parse_calls/executions) > 1
+
+SET LINESIZE 32767
+SET PAGESIZE 50000
+
+COLUMN sql_id FORMAT a13
+COLUMN sql_text FORMAT a70
+COLUMN parse_calls/executions FORMAT 999,999,999,999,999 HEADING 'parse_calls|executions'
+COLUMN parse_app_rank FORMAT 999 HEADING 'parse_app|rank'
+
+SELECT *
+FROM (SELECT sql_id
+             , sql_text
+             , parse_calls/executions
+             , DENSE_RANK() OVER (ORDER BY DECODE(executions, 0, 0, parse_calls / executions) DESC) AS parse_app_rank
+      FROM v$sql
+      WHERE DECODE(executions, 0, 0, parse_calls / executions) > 1
      )
-where parse_app_rank <= 5;
-set linesize 80
-set pagesize 14
+WHERE parse_app_rank <= 5
+/
+
+SET LINESIZE 80
+SET PAGESIZE 14
