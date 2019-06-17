dafd2d0639ed1cc06c5ec7418669353d7fcd2afa
Quanwen Zhao <guestart@163.com>
2019-06-17 10:19:38 +0800

    Update hit_ratio_db_buffer_cache_2.sql

--- routine_inspection/hit_ratio_db_buffer_cache_2.sql
+++ routine_inspection/hit_ratio_db_buffer_cache_2.sql
@@ -46,12 +46,9 @@ SELECT
       || ', ' ||
       (SELECT value FROM v$parameter WHERE name = 'db_name')
       || ', ' ||
-      (SELECT Round((1 - SUM(DECODE(name, 'physical reads', value, 0))
-       / 
-       (SUM(DECODE(name, 'db block gets', value, 0)) 
-       + 
-       SUM(DECODE(name, 'consistent gets', value, 0))) ), 4) * 100 
-       || '%' AS hit_ratio 
+      (SELECT ROUND((1 - SUM(DECODE(name, 'physical reads', value, 0)) / (SUM(DECODE(name, 'db block gets', value, 0))
+       +
+       SUM(DECODE(name, 'consistent gets', value, 0))) ), 4) * 100 || '%' AS hit_ratio 
        FROM v$sysstat
       )
 FROM dual
