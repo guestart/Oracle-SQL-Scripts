483893750300b4a3f254dd6a3d2eec0d9c8e3da0
Quanwen Zhao <guestart@163.com>
2019-07-05 17:27:08 +0800

    Update bth_grt_sel.sql

--- grant/bth_grt_sel.sql
+++ grant/bth_grt_sel.sql
@@ -4,11 +4,11 @@ REM     Author:    Quanwen Zhao
 REM     Dated:     Jun 30, 2019
 REM
 REM     Purpose:
-REM         This SQL script uses to batch grant (only) select privilege on specific user (prod)'s all of tables to
-REM         a new user (qwz), and then execute SPOOL sql file 'gen_bth_grt_sel.sql' to achieve the function of
-REM         'batch grant select'.
+REM         This SQL script uses to batch generate "grant (only) select privilege on specific user (prod)'s all 
+REM         of tables to  a new user (qwz)", and then execute SPOOL sql file 'gen_bth_grt_sel.sql' to achieve the
+REM         function of 'batch grant select'.
 REM
- 
+
 DROP USER qwz;
 CREATE USER qwz IDENTIFIED BY qwz;
 GRANT connect, resource TO qwz;
