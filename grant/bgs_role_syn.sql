ded8424198eec9af61567ae27b82b07888ff6831
Quanwen Zhao <guestart@163.com>
2019-07-06 11:29:34 +0800

    Update bgs_role_syn.sql

--- grant/bgs_role_syn.sql
+++ grant/bgs_role_syn.sql
@@ -4,13 +4,18 @@ REM     Author:    Quanwen Zhao
 REM     Dated:     Jul 03, 2019
 REM
 REM     Purpose:
-REM         This SQL script uses to batch generate "grant (only) select privilege on specific user (prod)'s all of tables to
-REM         a new role (prod)" and also batch generate "create the name of public synonym for original table name" on schema 'PROD', 
-REM         next grant new role (prod) to new user (qwz) on schema 'SYS', finally execute SPOOL sql file 'gen_bgs_role_syn.sql'
-REM         on schema 'PROD' to achieve the function of 'batch grant select'.
+REM         This SQL script uses to batch grant (only) select privilege on specific user (prod)'s all of tables to
+REM         a new role (prod) and then grant this role to new user (qwz). The order of executing steps are as follws:
 REM
-REM         The advantage and convenience of this approach is that it could not only grant more than one user but also just 
-REM         revoke role when revoking.
+REM         (1) Create new user 'qwz' and grant connect, resource to it on schema 'SYS';
+REM         (2) Create new role 'prod' on schema 'SYS';
+REM         (3) Grant create and drop public synonym to schema 'PROD' on schema 'SYS';
+REM         (4) Batch generate "grant (only) select privilege on schema (prod)'s all of tables to a new role (prod)" on schema 'PROD';
+REM         (5) Also batch generate "create the name of public synonym for original table name" on schema 'PROD';
+REM         (6) Execute SPOOL sql file 'gen_bgs_role_syn.sql' on schema 'PROD';
+REM         (7) grant new role (prod) to new user (qwz) on schema 'SYS';
+REM
+REM         The advantage and convenience of this approach is that it could not only grant more than one user but also just revoke role.
 REM
 
 SET long     10000
