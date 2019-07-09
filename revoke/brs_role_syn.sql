03ba89066f99093bee953eab893cc8691e3f31c1
Quanwen Zhao <guestart@163.com>
2019-07-09 14:50:18 +0800

    Update brs_role_syn.sql

--- revoke/brs_role_syn.sql
+++ revoke/brs_role_syn.sql
@@ -7,13 +7,12 @@ REM     Purpose:
 REM         This SQL script uses to revoke new role (prod) from new user (qwz) to whom if (once) being granted on schema 'SYS'.
 REM
 REM         If you wanna furthermore revoke select privilege on new role (prod) and drop this role you can do the following steps:
-REM
-REM         (1) Batch generate "revoke (only) select privilege on specific user (prod)'s all of tables from a new role (prod)" on schema 'PROD';
-REM         (2) Also batch generate "drop the name of public synonym for original table name" on schema 'PROD';
-REM         (3) Execute SPOOL sql file 'gen_brs_role_syn.sql' on schema 'PROD';
-REM         (4) Drop role 'prod' on schema 'SYS';
-REM         (5) Revoke 'drop public synonym' from schema 'PROD' on schema 'SYS';
-REM         (6) Revoke 'create publicc synonym' from schema 'PROD' on schema 'SYS';
+REM           (1) Batch generate "revoke (only) select privilege on specific user (prod)'s all of tables from a new role (prod)" on schema 'PROD';
+REM           (2) Also batch generate "drop the name of public synonym for original table name" on schema 'PROD';
+REM           (3) Execute SPOOL sql file 'gen_brs_role_syn.sql' on schema 'PROD';
+REM           (4) Drop role 'prod' on schema 'SYS';
+REM           (5) Revoke 'drop public synonym' from schema 'PROD' on schema 'SYS';
+REM           (6) Revoke 'create publicc synonym' from schema 'PROD' on schema 'SYS';
 REM
 REM         The advantage and convenience of this approach is that it could not only revoke more than one user but also just revoke role.
 REM
@@ -31,13 +30,13 @@ SET trimout   ON
 SET trimspool ON
 
 PROMPT =========================
-PROMPT Executing on <SYS> schema
+PROMPT Executing on "SYS" schema
 PROMPT =========================
 
 REVOKE prod FROM qwz;
 
 PROMPT ==========================
-PROMPT Executing on <PROD> schema
+PROMPT Executing on "PROD" schema
 PROMPT ==========================
 
 -- switching to specific schema "prod", BTW I use Oracle SEPS (Security External Pasword Store) to achieve the intention
@@ -66,7 +65,7 @@ SPOOL off
 @gen_brs_role_syn.sql;
 
 PROMPT =========================
-PROMPT Executing on <SYS> schema
+PROMPT Executing on "SYS" schema
 PROMPT =========================
 
 CONN / as sysdba;
