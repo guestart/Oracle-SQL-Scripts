7c6b03f7553e40e0ee84c71cb337a6433eee9be2
Quanwen Zhao <guestart@163.com>
2019-07-24 14:38:51 +0800

    Update brst2_scheduler.sql

--- plsql/brst2_scheduler.sql
+++ plsql/brst2_scheduler.sql
@@ -22,8 +22,12 @@ BEGIN
   DBMS_SCHEDULER.create_job (
      job_name           =>  'BRST2_JOB',
      job_type           =>  'STORED_PROCEDURE',
-     job_action         =>  'brgs_role_syn_tab_2;',
-     start_date         =>  '23-JUL-2019 05:00:00 PM China/Beijing',
+  -- job_action         =>  'brgs_role_syn_tab_2;', using this parameter, return ORA-27452: brgs_role_syn_tab_2; is an invalid name for a database object.
+     job_action         =>  'brgs_role_syn_tab_2', -- when eliminating semi-colon that is at the end of value, it's okay
+  -- start_date         =>  '23-JUL-2019 05:00:00 PM China/Beijing', using this parameter, return ORA-01882: timezone region not found
+  -- start_date         =>  TO_DATE('2019-07-23 05:00:00', 'yyyy-mm-dd hh24:mi:ss'), using this parameter, it's okay
+  -- start_date         =>  SYSDATE, using this parameter, it's okay
+     start_date         =>  SYSTIMESTAMP, -- using this parameter, it's okay
      repeat_interval    =>  'FREQ=DAILY; BYHOUR=4;',
      end_date           =>  NULL,
      auto_drop          =>  false,
