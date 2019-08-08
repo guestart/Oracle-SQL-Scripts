fe8f38c875a5c0ce3c9c83cc192b71b088ddfc7e
Quanwen Zhao <guestart@163.com>
2019-08-08 14:43:03 +0800

    Update user_scheduler_job_log.sql

--- scheduler/user_scheduler_job_log.sql
+++ scheduler/user_scheduler_job_log.sql
@@ -16,30 +16,54 @@ CONN test/test;
 SET LINESIZE 200
 SET PAGESIZE 50
 
-COLUMN  log_date  FORMAT  a35
-COLUMN  owner     FORMAT  a12
-COLUMN  status    FORMAT  a12
+COLUMN job_name  FORMAT a12
+COLUMN owner     FORMAT a12
+COLUMN log_date  FORMAT a35
+COLUMN job_class FORMAT a17
+COLUMN operation FORMAT a9
+COLUMN status    FORMAT a9
 
-SELECT log_id
-       , log_date
+SELECT job_name
        , owner
+       , log_id
+       , log_date
+       , job_class
+       , operation
        , status
 FROM user_scheduler_job_log
--- WHERE job_name = 'RTS_JOB'
 ORDER BY log_date
 /
 
 -- A detailed demo.
 -- 
--- TEST@xxxx> select log_id, log_date, owner, status from user_scheduler_job_log order by 2;
+-- TEST@xxxx> SET LINESIZE 200
+-- TEST@xxxx> SET PAGESIZE 50
+-- TEST@xxxx> 
+-- TEST@xxxx> COLUMN job_name  FORMAT a12
+-- TEST@xxxx> COLUMN owner     FORMAT a12
+-- TEST@xxxx> COLUMN log_date  FORMAT a35
+-- TEST@xxxx> COLUMN job_class FORMAT a17
+-- TEST@xxxx> COLUMN operation FORMAT a9
+-- TEST@xxxx> COLUMN status    FORMAT a9
+-- TEST@xxxx> 
+-- TEST@xxxx> SELECT job_name
+--   2               , owner
+--   3               , log_id
+--   4               , log_date
+--   5               , job_class
+--   6               , operation
+--   7               , status
+--   8  FROM user_scheduler_job_log
+--   9  ORDER BY log_date
+--  10  /
 -- 
---     LOG_ID LOG_DATE                            OWNER        STATUS
--- ---------- ----------------------------------- ------------ ------------
---      90900 06-AUG-19 04.36.09.219513 PM +08:00 TEST         SUCCEEDED
---      90901 06-AUG-19 04.41.09.042880 PM +08:00 TEST         SUCCEEDED
---      90902 06-AUG-19 04.46.09.048793 PM +08:00 TEST         SUCCEEDED
---      90904 06-AUG-19 04.51.09.044790 PM +08:00 TEST         SUCCEEDED
---      90905 06-AUG-19 04.56.09.046098 PM +08:00 TEST         SUCCEEDED
---      90906 06-AUG-19 04.56.09.048318 PM +08:00 TEST         SUCCEEDED
+-- JOB_NAME     OWNER            LOG_ID LOG_DATE                            JOB_CLASS         OPERATION STATUS
+-- ------------ ------------ ---------- ----------------------------------- ----------------- --------- ---------
+-- RTS_JOB      TEST              90900 06-AUG-19 04.36.09.219513 PM +08:00 DEFAULT_JOB_CLASS COMPLETED SUCCEEDED
+-- RTS_JOB      TEST              90901 06-AUG-19 04.41.09.042880 PM +08:00 DEFAULT_JOB_CLASS COMPLETED SUCCEEDED
+-- RTS_JOB      TEST              90902 06-AUG-19 04.46.09.048793 PM +08:00 DEFAULT_JOB_CLASS COMPLETED SUCCEEDED
+-- RTS_JOB      TEST              90904 06-AUG-19 04.51.09.044790 PM +08:00 DEFAULT_JOB_CLASS COMPLETED SUCCEEDED
+-- RTS_JOB      TEST              90905 06-AUG-19 04.56.09.046098 PM +08:00 DEFAULT_JOB_CLASS COMPLETED SUCCEEDED
+-- RTS_JOB      TEST              90906 06-AUG-19 04.56.09.048318 PM +08:00 DEFAULT_JOB_CLASS COMPLETED SUCCEEDED
 -- 
 -- 6 rows selected.
