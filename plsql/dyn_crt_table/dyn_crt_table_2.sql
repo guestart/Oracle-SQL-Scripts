9d608ca27833dcfbc374b4b21e0f26dd81a67b00
Quanwen Zhao <guestart@163.com>
2019-12-12 15:27:55 +0800

    Update dyn_crt_table_2.sql

--- plsql/dyn_crt_table/dyn_crt_table_2.sql
+++ plsql/dyn_crt_table/dyn_crt_table_2.sql
@@ -14,15 +14,19 @@ REM        SQL>
 REM        SQL> PROMPT ===========================================================================
 REM        SQL> PROMPT Creating a test table, according to the value of variable "l_num" you input
 REM        SQL> PROMPT ===========================================================================
+REM
 REM        SQL> 
 REM        SQL> DROP TABLE t PURGE;
-REM        
-REM        ACCEPT l_num NUMBER PROMPT 'Please input a value of l_num: ';
-REM        
-REM        
-REM        Table dropped.
-REM        
-REM        SQL> SQL> Please input a value of l_num: SQL> CREATE TABLE t
+REM        DROP TABLE t PURGE
+REM                   *
+REM        ERROR at line 1:
+REM        ORA-00942: table or view does not exist
+REM
+REM
+REM        SQL> 
+REM        SQL> ACCEPT l_num NUMBER PROMPT 'Please input a value of l_num: ';
+REM        Please input a value of l_num: 
+REM        SQL> CREATE TABLE t
 REM          2  SEGMENT CREATION IMMEDIATE
 REM          3  NOLOGGING
 REM          4  AS
@@ -30,27 +34,27 @@ REM          5     SELECT ROWNUM id
 REM          6            , CASE WHEN ROWNUM BETWEEN 1            AND 1/5*(&l_num) THEN 'low'
 REM          7                   WHEN ROWNUM BETWEEN 2/5*(&l_num) AND 3/5*(&l_num) THEN 'mid'
 REM          8                   WHEN ROWNUM BETWEEN 4/5*(&l_num) AND     (&l_num) THEN 'high'
-REM          9                   ELSE 'unknown' 
+REM          9                   ELSE 'unknown'
 REM         10              END flag
 REM         11            , DBMS_RANDOM.string ('p', 8) pwd
 REM         12     FROM XMLTABLE ('1 to xs:integer($i)' passing &l_num as "i")
 REM         13  ;
-REM        
+REM
 REM        Table created.
-REM        
+REM
 REM        SQL> desc t
 REM         Name                                      Null?    Type
 REM         ----------------------------------------- -------- ----------------------------
 REM         ID                                                 NUMBER
 REM         FLAG                                               VARCHAR2(7)
 REM         PWD                                                VARCHAR2(4000)
-REM        
+REM
 REM        SQL> select count(*) from t;
-REM        
+REM
 REM          COUNT(*)
 REM        ----------
 REM                 0
-REM        
+REM
 REM        SQL> 
 REM
 
