4b1bb29f4fb6ab8a01240886e27e33db931bec0d
Quanwen Zhao <guestart@163.com>
2020-06-22 09:05:59 +0800

    Update per_machine_act_conn_num_aggr_by_user.sql

--- routine_inspection/per_machine_act_conn_num_aggr_by_user.sql
+++ routine_inspection/per_machine_act_conn_num_aggr_by_user.sql
@@ -11,7 +11,10 @@ REM
 REM     Script:        per_machine_act_conn_num_aggr_by_user.sql
 REM     Author:        Quanwen Zhao
 REM     Dated:         May 11, 2019
-REM     Updated:       May 15, 2019                                                   
+REM     Updated:       May 15, 2019
+REM     Updated:       Jun 22, 2020
+                       Eliminating the WHERE clause of "username" and "status", only retaining the WHERE clause of "program",
+                       otherwise, being not able to find the important info connecting to Oracle Database by App Server.
 REM
 REM     Purpose:  
 REM       This sql script shows per machine's active connect numbers after aggregating by username on v$session,
@@ -31,9 +34,9 @@ SELECT username
        , status
        , count(*)
 FROM v$session
-WHERE username IS NOT NULL
---AND   status = 'ACTIVE'
-AND program = 'JDBC Thin Client'
+-- WHERE username IS NOT NULL
+-- AND   status = 'ACTIVE'
+WHERE program = 'JDBC Thin Client'
 GROUP BY username
          , machine
          , client_info
