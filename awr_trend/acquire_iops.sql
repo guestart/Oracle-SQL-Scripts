9a6bf0e08bd153a518f004de615e6fbd4c562e9a
Quanwen Zhao <guestart@163.com>
2021-09-24 14:33:34 +0800

    Update acquire_iops.sql

--- awr_trend/acquire_iops.sql
+++ awr_trend/acquire_iops.sql
@@ -3,15 +3,42 @@ REM     Script:        acquire_iops.sql
 REM     Author:        Quanwen Zhao
 REM     Dated:         Sep 23, 2021
 REM
+REM     Updated:       Sep 24, 2021
+REM       Searched out the MoS article "How to Calculate the Number of IOPS and Throughput of a Database (Doc ID 2206831.1)" from Google,
+REM       it told us IOPS can be found in different places of the AWR report:
+REM         (1) Instance Activity Stats
+REM         (2) IO Profile (Starting with 11gR2)
+REM         (3) Load Profile
+REM
+REM       Instance Activity Stats:
+REM       IOPS - (Input/Output Operations Per Second) - This is the sum of "Physical Read Total IO Requests" and "Physical Write Total IO Requests".
+REM
+REM       IO Profile:
+REM       IOPS - Total Requests (This value is the sum of the metrics "Physical Read Total IO Requests Per Sec" and "Physical Write Total IO Requests Per Sec"
+REM       from the Instance Activity Stats area).
+REM
+REM       Load Profile:
+REM       Please note that the information displayed here is a subset of the one in the IO Profile area:
+REM
+REM       Load profile      -> Instance Activity Stats
+REM       Read IO requests  -> physical read IO requests
+REM       Write IO requests -> physical write IO requests
+REM       Read IO (MB)      -> physical read bytes
+REM       Write IO (MB)     -> physical write bytes
+REM
+REM       physical read IO requests:
+REM       Number of read requests for application activity (mainly buffer cache and direct load operation) which read one or more database blocks per request.
+REM       This is a subset of "physical read total IO requests" statistic.
+REM
 REM     Last tested:
 REM             11.2.0.4
 REM             19.3.0.0
 REM             21.3.0.0
 REM
 REM     Purpose:
-REM       Typically there saves the average value of "Physical Read IO Requests Per Sec" and "Physical Write IO Requests Per Sec"
+REM       Typically there saves the average value of "Physical Read Total IO Requests Per Sec" and "Physical Write Total IO Requests Per Sec"
 REM       in each of snap_id of the view "DBA_HIST_SYSMETRIC_SUMMARY" (in which the value of its column "metric_unit" is
-REM       "Physical Read IO Requests Per Sec" and "Physical Write IO Requests Per Sec"), here we use the analytic function
+REM       "Physical Read Total IO Requests Per Sec" and "Physical Write Total IO Requests Per Sec"), here we use the analytic function
 REM       "LAG () OVER()" to get the prior snap_id from current snap_id for more clearly showing "IOPS" between these two snap_id.
 REM
 REM     References:
@@ -31,7 +58,8 @@ FROM (
             , end_time
             , ROUND(SUM(average), 2) iops
        FROM dba_hist_sysmetric_summary
-       WHERE metric_name IN ('Physical Read IO Requests Per Sec', 'Physical Write IO Requests Per Sec')
+    -- WHERE metric_name IN ('Physical Read IO Requests Per Sec', 'Physical Write IO Requests Per Sec')
+       WHERE metric_name IN ('Physical Read Total IO Requests Per Sec', 'Physical Write Total IO Requests Per Sec')
        GROUP BY snap_id
               , instance_number
               , dbid
