96c50645c2b2979108ee6cfed381caaeed21baf1
Quanwen Zhao <guestart@163.com>
2021-09-27 15:42:29 +0800

    Update acquire_dbtime.sql

--- awr_trend/acquire_dbtime.sql
+++ awr_trend/acquire_dbtime.sql
@@ -27,6 +27,7 @@ REM       able to (inner) join the view DBA_HIST_SNAPSHOT and DBA_HIST_SYS_TIME_
 REM       and end_time of a snap_id.
 REM
 REM     Reference:
+REM       https://docs.oracle.com/en/database/oracle/oracle-database/19/refrn/DBA_HIST_SNAPSHOT.html#GUID-542B6CA6-793B-4D15-AAFD-4D3E6550C0B6
 REM       https://docs.oracle.com/en/database/oracle/oracle-database/19/refrn/DBA_HIST_SYS_TIME_MODEL.html#GUID-263D0396-7C98-4C26-9993-DCC42EA9E87E
 REM       http://blog.itpub.net/28602568/viewspace-1467897/
 REM
