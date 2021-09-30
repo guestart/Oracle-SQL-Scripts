74d94656129adfa8b081d0df13bb6a52dc545aa8
Quanwen Zhao <guestart@163.com>
2021-09-30 20:14:31 +0800

    Update acquire_lps.sql

--- awr_trend/acquire_lps.sql
+++ awr_trend/acquire_lps.sql
@@ -15,6 +15,19 @@ REM
 REM       Next we use the analytic function "LAG () OVER()" to get the prior snap_id from current snap_id for more
 REM       clearly showing "Logons Per Sec" between these two snap_id.
 REM
+REM       SET LINESIZE 80
+REM       DESC acquire_awr_lps
+REM        Name                                      Null?    Type
+REM        ----------------------------------------- -------- ----------------------------
+REM        INSTANCE_NUMBER                           NOT NULL NUMBER
+REM        FIRST_SNAP_ID                             NOT NULL NUMBER
+REM        SECOND_SNAP_ID                            NOT NULL NUMBER
+REM        BEGIN_TIME                                NOT NULL DATE
+REM        END_TIME                                  NOT NULL DATE
+REM        METRIC_NAME                               NOT NULL VARCHAR2(18)
+REM        METRIC_UNIT                               NOT NULL VARCHAR2(18)
+REM        AWR_LPS                                            NUMBER
+REM
 REM     References:
 REM       https://docs.oracle.com/en/database/oracle/oracle-database/19/refrn/DBA_HIST_SYSMETRIC_HISTORY.html#GUID-4A9988AE-B1B5-4E71-9C38-C95448B3F758
 REM       https://docs.oracle.com/en/database/oracle/oracle-database/19/refrn/DBA_HIST_SYSMETRIC_SUMMARY.html#GUID-E6377E5F-1FFF-4563-850F-C361B9D85048
@@ -23,8 +36,9 @@ REM
 SET LINESIZE 200
 SET PAGESIZE 200
 
-COLUMN metric_name FORMAT a15
-COLUMN metric_unit FORMAT a15
+COLUMN metric_name FORMAT a18
+COLUMN metric_unit FORMAT a18
+COLUMN awr_lps     FORMAT 999,999,999.99
 
 ALTER SESSION SET nls_date_format = 'yyyy-mm-dd hh24:mi:ss';
 
@@ -32,12 +46,13 @@ WITH dhsh
 AS (
      SELECT *
      FROM (
-            SELECT LAG(snap_id, 1, 0) OVER(PARTITION BY dbid, instance_number ORDER BY snap_id) first_snap_id
+            SELECT instance_number
+                 , LAG(snap_id, 1, 0) OVER(PARTITION BY dbid, instance_number ORDER BY snap_id) first_snap_id
                  , snap_id second_snap_id
                  , MIN(begin_time) begin_time
                  , MAX(end_time) end_time
-              -- , intsize
                  , metric_name
+                 , metric_unit
                  , SUM(value*(intsize/1e2)) logon_numbers
                  , (MAX(end_time)-MIN(begin_time))*24*36e2 interval_secs
             FROM dba_hist_sysmetric_history
@@ -46,16 +61,20 @@ AS (
                    , instance_number
                    , snap_id
                    , metric_name
-            ORDER BY snap_id
+                   , metric_unit
+            ORDER BY instance_number
+                   , first_snap_id
           )
      WHERE first_snap_id <> 0
    )
-SELECT first_snap_id
+SELECT instance_number
+     , first_snap_id
      , second_snap_id
      , begin_time
      , end_time
      , metric_name
-     , ROUND(logon_numbers/interval_secs, 2) lps
+     , metric_unit
+     , ROUND(logon_numbers/interval_secs, 2) awr_lps
 FROM dhsh
 ;
 
@@ -63,16 +82,18 @@ or
 
 SELECT *
 FROM (
-       SELECT LAG(snap_id, 1, 0) OVER(PARTITION BY dbid, instance_number ORDER BY snap_id) first_snap_id
+       SELECT instance_number
+            , LAG(snap_id, 1, 0) OVER(PARTITION BY dbid, instance_number ORDER BY snap_id) first_snap_id
             , snap_id second_snap_id
             , begin_time
             , end_time
             , metric_name
-         -- , metric_unit
-            , ROUND(average, 2) lps
+            , metric_unit
+            , ROUND(average, 2) awr_lps
        FROM dba_hist_sysmetric_summary
        WHERE metric_name = 'Logons Per Sec'
-       ORDER BY snap_id
+       ORDER BY instance_number
+              , first_snap_id
      )
 WHERE first_snap_id <> 0
 ;
