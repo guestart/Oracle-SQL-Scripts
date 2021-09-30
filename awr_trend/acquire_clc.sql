c215ffbd5d63511636dc7e60f64d8032a97e8497
Quanwen Zhao <guestart@163.com>
2021-09-30 10:36:35 +0800

    Update acquire_clc.sql

--- awr_trend/acquire_clc.sql
+++ awr_trend/acquire_clc.sql
@@ -14,6 +14,19 @@ REM
 REM       Next we use the analytic function "LAG () OVER()" to get the prior snap_id from current snap_id for more
 REM       clearly showing "Current Logons Count" between these two snap_id.
 REM
+REM       SET LINESIZE 80
+REM       DESC acquire_awr_clc
+REM        Name                                      Null?    Type
+REM        ----------------------------------------- -------- ----------------------------
+REM        INSTANCE_NUMBER                           NOT NULL NUMBER
+REM        FIRST_SNAP_ID                             NOT NULL NUMBER
+REM        SECOND_SNAP_ID                            NOT NULL NUMBER
+REM        BEGIN_TIME                                NOT NULL DATE
+REM        END_TIME                                  NOT NULL DATE
+REM        METRIC_NAME                               NOT NULL VARCHAR2(20)
+REM        METRIC_UNIT                               NOT NULL VARCHAR2(12)
+REM        AWR_CLC                                            NUMBER
+REM
 REM     References:
 REM       https://docs.oracle.com/en/database/oracle/oracle-database/19/refrn/DBA_HIST_SYSMETRIC_SUMMARY.html#GUID-E6377E5F-1FFF-4563-850F-C361B9D85048
 REM
@@ -22,22 +35,27 @@ SET LINESIZE 200
 SET PAGESIZE 200
 
 COLUMN metric_name FORMAT a20
-COLUMN metric_unit FORMAT a20
+COLUMN metric_unit FORMAT a12
+COLUMN awr_clc     FORMAT 999,999.99
 
 ALTER SESSION SET nls_date_format = 'yyyy-mm-dd hh24:mi:ss';
 
+CREATE OR REPLACE VIEW acquire_awr_clc
+AS
 SELECT *
 FROM (
-       SELECT LAG(snap_id, 1, 0) OVER(PARTITION BY dbid, instance_number ORDER BY snap_id) first_snap_id
+       SELECT instance_number
+            , LAG(snap_id, 1, 0) OVER(PARTITION BY dbid, instance_number ORDER BY snap_id) first_snap_id
             , snap_id second_snap_id
             , begin_time
             , end_time
             , metric_name
-         -- , metric_unit
-            , ROUND(average, 2) clc
+            , metric_unit
+            , ROUND(average, 2) awr_clc
        FROM dba_hist_sysmetric_summary
        WHERE metric_name = 'Current Logons Count'
-       ORDER BY snap_id
+       ORDER BY instance_number
+              , first_snap_id
      )
 WHERE first_snap_id <> 0
 ;
