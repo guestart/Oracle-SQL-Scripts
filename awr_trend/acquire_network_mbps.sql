d2471fb643ea1ca79b745143b21518fddab00156
Quanwen Zhao <guestart@163.com>
2021-09-30 20:35:18 +0800

    Update acquire_network_mbps.sql

--- awr_trend/acquire_network_mbps.sql
+++ awr_trend/acquire_network_mbps.sql
@@ -16,6 +16,19 @@ REM
 REM       Next we use the analytic function "LAG () OVER()" to get the prior snap_id from current snap_id for more
 REM       clearly showing "Network MBPS" between these two snap_id.
 REM
+REM       SET LINESIZE 80
+REM       DESC acquire_network_mbps
+REM        Name                                      Null?    Type
+REM        ----------------------------------------- -------- ----------------------------
+REM        INSTANCE_NUMBER                           NOT NULL NUMBER
+REM        FIRST_SNAP_ID                             NOT NULL NUMBER
+REM        SECOND_SNAP_ID                            NOT NULL NUMBER
+REM        BEGIN_TIME                                NOT NULL DATE
+REM        END_TIME                                  NOT NULL DATE
+REM        METRIC_NAME                               NOT NULL VARCHAR2(30)
+REM        METRIC_UNIT                               NOT NULL VARCHAR2(16)
+REM        NETWORK_MBPS                                       NUMBER
+REM
 REM     References:
 REM       https://docs.oracle.com/en/database/oracle/oracle-database/19/refrn/DBA_HIST_SYSMETRIC_HISTORY.html#GUID-4A9988AE-B1B5-4E71-9C38-C95448B3F758
 REM       https://docs.oracle.com/en/database/oracle/oracle-database/19/refrn/DBA_HIST_SYSMETRIC_SUMMARY.html#GUID-E6377E5F-1FFF-4563-850F-C361B9D85048
@@ -24,8 +37,9 @@ REM
 SET LINESIZE 200
 SET PAGESIZE 200
 
-COLUMN metric_name FORMAT a25
-COLUMN metric_unit FORMAT a25
+COLUMN metric_name FORMAT a30
+COLUMN metric_unit FORMAT a16
+COLUMN network_mbps FORMAT 999,999,999.99
 
 ALTER SESSION SET nls_date_format = 'yyyy-mm-dd hh24:mi:ss';
 
@@ -33,12 +47,13 @@ WITH dhsh
 AS (
      SELECT *
      FROM (
-            SELECT LAG(snap_id, 1, 0) OVER(PARTITION BY dbid, instance_number ORDER BY snap_id) first_snap_id
+            SELECT instance_number
+                 , LAG(snap_id, 1, 0) OVER(PARTITION BY dbid, instance_number ORDER BY snap_id) first_snap_id
                  , snap_id second_snap_id
                  , MIN(begin_time) begin_time
                  , MAX(end_time) end_time
-              -- , intsize
-              -- , metric_name
+                 , metric_name
+                 , metric_unit
                  , SUM((value/POWER(2, 20))*(intsize/1e2)) network_mb_size
                  , (MAX(end_time)-MIN(begin_time))*24*36e2 interval_secs
             FROM dba_hist_sysmetric_history
@@ -46,14 +61,20 @@ AS (
             GROUP BY dbid
                    , instance_number
                    , snap_id
-            ORDER BY snap_id
+                   , metric_name
+                   , metric_unit
+            ORDER BY instance_number
+                   , first_snap_id
           )
      WHERE first_snap_id <> 0
    )
-SELECT first_snap_id
+SELECT instance_number
+     , first_snap_id
      , second_snap_id
      , begin_time
      , end_time
+     , metric_name
+     , metric_unit
      , ROUND(network_mb_size/interval_secs, 2) network_mbps
 FROM dhsh
 ;
@@ -62,17 +83,19 @@ or
 
 SELECT *
 FROM (
-       SELECT LAG(snap_id, 1, 0) OVER(PARTITION BY dbid, instance_number ORDER BY snap_id) first_snap_id
+       SELECT instance_number
+            , LAG(snap_id, 1, 0) OVER(PARTITION BY dbid, instance_number ORDER BY snap_id) first_snap_id
             , snap_id second_snap_id
             , begin_time
             , end_time
-         -- , metric_name
-         -- , metric_unit
+            , metric_name
+            , metric_unit
             , ROUND(average/POWER(2, 20), 2) network_mbps -- metric_unit is "CentiSeconds Per Second" so average should divide by 1e2.
        FROM dba_hist_sysmetric_summary
     -- WHERE metric_name = 'Network Bytes Per Sec' -- not "Network Bytes Per Sec", should the following metric_name "Network Traffic Volume Per Sec".
        WHERE metric_name = 'Network Traffic Volume Per Sec'
-       ORDER BY snap_id
+       ORDER BY instance_number
+              , first_snap_id
      )
 WHERE first_snap_id <> 0
 ;
