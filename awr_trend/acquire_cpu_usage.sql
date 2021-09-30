e6b46029e80526710dc9b08be6cf77ec2aeac4c2
Quanwen Zhao <guestart@163.com>
2021-09-30 13:48:49 +0800

    Update acquire_cpu_usage.sql

--- awr_trend/acquire_cpu_usage.sql
+++ awr_trend/acquire_cpu_usage.sql
@@ -14,6 +14,19 @@ REM
 REM       Next we use the analytic function "LAG () OVER()" to get the prior snap_id from current snap_id for more
 REM       clearly showing "Host CPU Utilization (%)" between these two snap_id.
 REM
+REM       SET LINESIZE 80
+REM       DESC acquire_awr_cpu_usage
+REM        Name                                      Null?    Type
+REM        ----------------------------------------- -------- ----------------------------
+REM        INSTANCE_NUMBER                           NOT NULL NUMBER
+REM        FIRST_SNAP_ID                             NOT NULL NUMBER
+REM        SECOND_SNAP_ID                            NOT NULL NUMBER
+REM        BEGIN_TIME                                NOT NULL DATE
+REM        END_TIME                                  NOT NULL DATE
+REM        METRIC_NAME                               NOT NULL VARCHAR2(25)
+REM        METRIC_UNIT                               NOT NULL VARCHAR2(20)
+REM        AWR_CPU_USAGE                                      VARCHAR2(13)
+REM
 REM     References:
 REM       https://docs.oracle.com/en/database/oracle/oracle-database/19/refrn/DBA_HIST_SYSMETRIC_SUMMARY.html#GUID-E6377E5F-1FFF-4563-850F-C361B9D85048
 REM
@@ -21,23 +34,26 @@ REM
 SET LINESIZE 200
 SET PAGESIZE 200
 
-COLUMN metric_name FORMAT a25
-COLUMN metric_unit FORMAT a25
+COLUMN metric_name   FORMAT a25
+COLUMN metric_unit   FORMAT a20
+COLUMN awr_cpu_usage FORMAT a13
 
 ALTER SESSION SET nls_date_format = 'yyyy-mm-dd hh24:mi:ss';
 
 SELECT *
 FROM (
-       SELECT LAG(snap_id, 1, 0) OVER(PARTITION BY dbid, instance_number ORDER BY snap_id) first_snap_id
+       SELECT instance_number
+            , LAG(snap_id, 1, 0) OVER(PARTITION BY dbid, instance_number ORDER BY snap_id) first_snap_id
             , snap_id second_snap_id
             , begin_time
             , end_time
             , metric_name
-         -- , metric_unit
-            , ROUND(average, 2) || '%' cpu_usage
+            , metric_unit
+            , ROUND(average, 2) || '%' awr_cpu_usage
        FROM dba_hist_sysmetric_summary
        WHERE metric_name = 'Host CPU Utilization (%)'
-       ORDER BY snap_id
+       ORDER BY instance_number
+              , first_snap_id
      )
 WHERE first_snap_id <> 0
 ;
