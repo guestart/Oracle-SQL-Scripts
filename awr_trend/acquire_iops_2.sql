29c513cdca09a3c0b549ab6e82c9e9a56242989a
Quanwen Zhao <guestart@163.com>
2021-09-30 15:45:33 +0800

    Update acquire_iops_2.sql

--- awr_trend/acquire_iops_2.sql
+++ awr_trend/acquire_iops_2.sql
@@ -12,6 +12,19 @@ REM     Purpose:
 REM       We can get "IOPS" from the metric_name "I/O Requests per Second" of the view "DBA_HIST_SYSMETRIC_HISTORY"
 REM       or "DBA_HIST_SYSMETRIC_SUMMARY".
 REM
+REM       SET LINESIZE 80
+REM       DESC acquire_iops
+REM        Name                                      Null?    Type
+REM        ----------------------------------------- -------- ----------------------------
+REM        INSTANCE_NUMBER                           NOT NULL NUMBER
+REM        FIRST_SNAP_ID                             NOT NULL NUMBER
+REM        SECOND_SNAP_ID                            NOT NULL NUMBER
+REM        BEGIN_TIME                                NOT NULL DATE
+REM        END_TIME                                  NOT NULL DATE
+REM        METRIC_NAME                               NOT NULL VARCHAR2(25)
+REM        METRIC_UNIT                               NOT NULL VARCHAR2(20)
+REM        IOPS                                               NUMBER
+REM
 REM     References:
 REM       https://docs.oracle.com/en/database/oracle/oracle-database/19/refrn/DBA_HIST_SYSMETRIC_HISTORY.html#GUID-4A9988AE-B1B5-4E71-9C38-C95448B3F758
 REM       https://docs.oracle.com/en/database/oracle/oracle-database/19/refrn/DBA_HIST_SYSMETRIC_SUMMARY.html#GUID-E6377E5F-1FFF-4563-850F-C361B9D85048
@@ -20,18 +33,23 @@ REM
 SET LINESIZE 200
 SET PAGESIZE 200
 
+COLUMN metric_name FORMAT a25
+COLUMN metric_unit FORMAT a20
+COLUMN iops        FORMAT 999,999,999.99
+
 ALTER SESSION SET nls_date_format = 'yyyy-mm-dd hh24:mi:ss';
 
 WITH dhsh
 AS (
      SELECT *
      FROM (
-            SELECT LAG(snap_id, 1, 0) OVER(PARTITION BY dbid, instance_number ORDER BY snap_id) first_snap_id
+            SELECT instance_number
+                 , LAG(snap_id, 1, 0) OVER(PARTITION BY dbid, instance_number ORDER BY snap_id) first_snap_id
                  , snap_id second_snap_id
                  , MIN(begin_time) begin_time
                  , MAX(end_time) end_time
-              -- , intsize
-              -- , metric_name
+                 , metric_name
+                 , metric_unit
                  , SUM(value*(intsize/1e2)) io_requests
                  , (MAX(end_time)-MIN(begin_time))*24*36e2 interval_secs
             FROM dba_hist_sysmetric_history
@@ -39,14 +57,20 @@ AS (
             GROUP BY dbid
                    , instance_number
                    , snap_id
-            ORDER BY snap_id
+                   , metric_name
+                   , metric_unit
+            ORDER BY instance_number
+                   , first_snap_id
           )
      WHERE first_snap_id <> 0
    )
-SELECT first_snap_id
+SELECT instance_number
+     , first_snap_id
      , second_snap_id
      , begin_time
      , end_time
+     , metric_name
+     , metric_unit
      , ROUND(io_requests/interval_secs, 2) iops
 FROM dhsh
 ;
@@ -55,14 +79,18 @@ or
 
 SELECT *
 FROM (
-       SELECT LAG(snap_id, 1, 0) OVER(PARTITION BY dbid, instance_number ORDER BY snap_id) first_snap_id
+       SELECT instance_number
+            , LAG(snap_id, 1, 0) OVER(PARTITION BY dbid, instance_number ORDER BY snap_id) first_snap_id
             , snap_id second_snap_id
             , begin_time
             , end_time
+            , metric_name
+            , metric_unit
             , ROUND(average, 2) iops
        FROM dba_hist_sysmetric_summary
        WHERE metric_name = 'I/O Requests per Second'
-       ORDER BY snap_id
+       ORDER BY instance_number
+              , first_snap_id
      )
 WHERE first_snap_id <> 0
 ;
