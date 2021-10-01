1588f4348f87f656dcfd34ffc604193f4b2d2741
Quanwen Zhao <guestart@163.com>
2021-10-01 20:26:35 +0800

    Create acquire_recent_tps.sql

---
+++ recent_metrics/acquire_recent_tps.sql
@@ -0,0 +1,49 @@
+REM
+REM     Script:        acquire_recent_tps.sql
+REM     Author:        Quanwen Zhao
+REM     Dated:         Oct 01, 2021
+REM
+REM     Last tested:
+REM             11.2.0.4
+REM             19.3.0.0
+REM             21.3.0.0
+REM
+REM     Purpose:
+REM       In general we can get metric_name "User Transaction Per Sec" and metric_unit "Transactions Per Second"
+REM       from the oracle dynamic performance view "v$sysmetric_history" and "v$sysmetric_summary".
+REM
+REM       There saves the "User Transaction Per Sec" with each interval one minute during the period of recent
+REM       one hour in the view "v$sysmetric_history" and there saves the "User Transaction Per Sec" with the
+REM       interval recent one hour in the view "v$sysmetric_summary".
+REM
+
+SET LINESIZE 200
+SET PAGESIZE 200
+
+COLUMN metric_name FORMAT a25
+COLUMN metric_unit FORMAT a25
+COLUMN recent_tps  FORMAT 999,999,999.99
+
+ALTER SESSION SET nls_date_format = 'yyyy-mm-dd hh24:mi:ss';
+
+SELECT begin_time
+     , end_time
+     , metric_name
+     , metric_unit
+     , ROUND(value, 2) recent_tps
+FROM v$sysmetric_history
+WHERE metric_name = 'User Transaction Per Sec'
+ORDER BY begin_time
+;
+
+or
+
+SELECT begin_time
+     , end_time
+     , metric_name
+     , metric_unit
+     , ROUND(average, 2) recent_tps
+FROM v$sysmetric_summary
+WHERE metric_name = 'User Transaction Per Sec'
+ORDER BY begin_time
+;
