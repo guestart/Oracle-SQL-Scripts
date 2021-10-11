b28f3249d708b1793a353fa5fde932360f0b531a
Quanwen Zhao <guestart@163.com>
2021-10-11 10:24:47 +0800

    Create acquire_recent_dbgpt.sql

---
+++ recent_metrics/acquire_recent_dbgpt.sql
@@ -0,0 +1,49 @@
+REM
+REM     Script:        acquire_recent_dbgpt.sql
+REM     Author:        Quanwen Zhao
+REM     Dated:         Oct 11, 2021
+REM
+REM     Last tested:
+REM             11.2.0.4
+REM             19.3.0.0
+REM             21.3.0.0
+REM
+REM     Purpose:
+REM       In general we can get metric_name "DB Block Gets Per Txn" and metric_unit "Blocks Per Txn"
+REM       from the oracle dynamic performance view "v$sysmetric_history" and "v$sysmetric_summary".
+REM
+REM       There saves the "DB Block Gets Per Txn" with each interval one minute during the period of recent
+REM       one hour in the view "v$sysmetric_history" and there saves the "DB Block Gets Per Txn" with the
+REM       interval recent one hour in the view "v$sysmetric_summary".
+REM
+
+SET LINESIZE 200
+SET PAGESIZE 200
+
+COLUMN metric_name FORMAT a25
+COLUMN metric_unit FORMAT a18
+COLUMN dbgpt       FORMAT 999,999.99
+
+ALTER SESSION SET nls_date_format = 'yyyy-mm-dd hh24:mi:ss';
+
+SELECT begin_time
+     , end_time
+     , metric_name
+     , metric_unit
+     , ROUND(value, 2) dbgpt
+FROM v$sysmetric_history
+WHERE metric_name = 'DB Block Gets Per Txn'
+ORDER BY begin_time
+;
+
+or
+
+SELECT begin_time
+     , end_time
+     , metric_name
+     , metric_unit
+     , ROUND(average, 2) dbgpt
+FROM v$sysmetric_summary
+WHERE metric_name = 'DB Block Gets Per Txn'
+ORDER BY begin_time
+;
