737284714a444347ddb49b1d9ee3f01ed2be3e78
Quanwen Zhao <guestart@163.com>
2021-10-19 10:54:30 +0800

    Update acquire_clc.sql

--- awr_trend/acquire_clc.sql
+++ awr_trend/acquire_clc.sql
@@ -3,6 +3,9 @@ REM     Script:        acquire_clc.sql
 REM     Author:        Quanwen Zhao
 REM     Dated:         Sep 28, 2021
 REM
+REM     Updated:       Oct 19, 2021
+REM                    Adding the code snippets visualizing the oracle performance metric "CLC" in the past and real time by the custom report of SQL Developer.
+REM
 REM     Last tested:
 REM             11.2.0.4
 REM             19.3.0.0
@@ -30,6 +33,144 @@ REM
 REM     References:
 REM       https://docs.oracle.com/en/database/oracle/oracle-database/19/refrn/DBA_HIST_SYSMETRIC_SUMMARY.html#GUID-E6377E5F-1FFF-4563-850F-C361B9D85048
 REM
+
+-- Current Logons Count in Last 31 Days (interval by each day).
+
+SET LINESIZE 200
+SET PAGESIZE 200
+
+COLUMN metric_name FORMAT a25
+COLUMN snap_date   FORMAT a12
+COLUMN clc         FORMAT 999,999.99
+
+ALTER SESSION SET nls_date_format = 'yyyy-mm-dd hh24:mi:ss';
+
+WITH clc_per_hour AS (
+SELECT TO_CHAR(end_time, 'yyyy-mm-dd') snap_date
+     , metric_name
+     , average
+FROM dba_hist_sysmetric_summary
+WHERE metric_name = 'Current Logons Count'
+AND   end_time >= SYSDATE - 30
+)
+SELECT snap_date                                    -- the group column
+     , metric_name                                  -- the series column
+     , ROUND(SUM(average)/COUNT(snap_date), 2) clc  -- the value column
+FROM clc_per_hour
+GROUP BY snap_date
+       , metric_name
+ORDER BY snap_date
+;
+
+-- Current Logons Count in Last 31 Days (interval by each hour).
+
+SET LINESIZE 200
+SET PAGESIZE 200
+
+COLUMN metric_name    FORMAT a25
+COLUMN snap_date_time FORMAT a20
+COLUMN clc            FORMAT 999,999.99
+
+ALTER SESSION SET nls_date_format = 'yyyy-mm-dd hh24:mi:ss';
+
+SELECT TO_CHAR(end_time, 'yyyy-mm-dd hh24:mi:ss') snap_date_time  -- the group column
+     , metric_name                                                -- the series column
+     , ROUND(average, 2) clc                                      -- the value column
+FROM dba_hist_sysmetric_summary
+WHERE metric_name = 'Current Logons Count'
+AND   end_time >= SYSDATE - 30
+ORDER BY snap_date_time
+;
+
+-- Current Logons Count in Last 7 Days (interval by each day).
+
+SET LINESIZE 200
+SET PAGESIZE 200
+
+COLUMN metric_name FORMAT a25
+COLUMN snap_date   FORMAT a12
+COLUMN clc         FORMAT 999,999.99
+
+ALTER SESSION SET nls_date_format = 'yyyy-mm-dd hh24:mi:ss';
+
+WITH clc_per_hour AS (
+SELECT TO_CHAR(end_time, 'yyyy-mm-dd') snap_date
+     , metric_name
+     , average
+FROM dba_hist_sysmetric_summary
+WHERE metric_name = 'Current Logons Count'
+AND   end_time >= SYSDATE - 6
+)
+SELECT snap_date                                    -- the group column
+     , metric_name                                  -- the series column
+     , ROUND(SUM(average)/COUNT(snap_date), 2) clc  -- the value column
+FROM clc_per_hour
+GROUP BY snap_date
+       , metric_name
+ORDER BY snap_date
+;
+
+-- Current Logons Count in Last 7 Days (interval by each hour).
+
+SET LINESIZE 200
+SET PAGESIZE 200
+
+COLUMN metric_name    FORMAT a25
+COLUMN snap_date_time FORMAT a20
+COLUMN clc            FORMAT 999,999.99
+
+ALTER SESSION SET nls_date_format = 'yyyy-mm-dd hh24:mi:ss';
+
+SELECT TO_CHAR(end_time, 'yyyy-mm-dd hh24:mi:ss') snap_date_time  -- the group column
+     , metric_name                                                -- the series column
+     , ROUND(average, 2) clc                                      -- the value column
+FROM dba_hist_sysmetric_summary
+WHERE metric_name = 'Current Logons Count'
+AND   end_time >= SYSDATE - 6
+ORDER BY snap_date_time
+;
+
+-- Current Logons Count in Last 24 Hours.
+
+SET LINESIZE 200
+SET PAGESIZE 200
+
+COLUMN metric_name    FORMAT a25
+COLUMN snap_date_time FORMAT a20
+COLUMN clc            FORMAT 999,999.99
+
+ALTER SESSION SET nls_date_format = 'yyyy-mm-dd hh24:mi:ss';
+
+SELECT TO_CHAR(end_time, 'yyyy-mm-dd hh24:mi:ss') snap_date_time  -- the group column
+     , metric_name                                                -- the series column
+     , ROUND(average, 2) clc                                      -- the value column
+FROM dba_hist_sysmetric_summary
+WHERE metric_name = 'Current Logons Count'
+AND   end_time >= SYSDATE - 1
+ORDER BY snap_date_time
+;
+
+-- Current Logons Count in Real Time.
+
+SET LINESIZE 200
+SET PAGESIZE 200
+
+COLUMN metric_name    FORMAT a25
+COLUMN snap_date_time FORMAT a20
+COLUMN clc            FORMAT 999,999.99
+
+ALTER SESSION SET nls_date_format = 'yyyy-mm-dd hh24:mi:ss';
+
+SELECT TO_CHAR(end_time, 'yyyy-mm-dd hh24:mi:ss') snap_date_time  -- the group column
+     , metric_name                                                -- the series column
+     , ROUND(value, 2) clc                                        -- the value column
+FROM v$sysmetric_history
+WHERE metric_name = 'Current Logons Count'
+AND   group_id = 2                                                -- just retrieve the name with "System Metrics Long Duration" in v$metricgroup
+ORDER BY snap_date_time
+;
+
+-- The original code.
 
 SET LINESIZE 200
 SET PAGESIZE 200
