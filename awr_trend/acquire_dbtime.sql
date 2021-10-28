306ea7eb8615bb383326f77e55f5f8ae8206fd6c
Quanwen Zhao <guestart@163.com>
2021-10-28 16:42:39 +0800

    Update acquire_dbtime.sql

--- awr_trend/acquire_dbtime.sql
+++ awr_trend/acquire_dbtime.sql
@@ -6,6 +6,8 @@ REM
 REM     Updated:       Sep 22, 2021
 REM                    (1) Adding the content of Purpose;
 REM                    (2) Rewritten SQL script to the format "WITH ... AS ()" so that more clearly reading and understanding;
+REM                    Oct 28, 2021
+REM                    Adding the code snippets visualizing the oracle performance metric "DB time" in the past and real time by the custom report of SQL Developer.
 REM
 REM     Last tested:
 REM             11.2.0.4
@@ -43,6 +45,431 @@ REM       https://docs.oracle.com/en/database/oracle/oracle-database/19/refrn/DB
 REM       https://docs.oracle.com/en/database/oracle/oracle-database/19/refrn/DBA_HIST_SYS_TIME_MODEL.html#GUID-263D0396-7C98-4C26-9993-DCC42EA9E87E
 REM       http://blog.itpub.net/28602568/viewspace-1467897/
 REM
+
+-- DB time in Last 31 Days (interval by each day).
+
+SET LINESIZE 200
+SET PAGESIZE 200
+
+COLUMN snap_date FORMAT a10
+COLUMN stat_name FORMAT a10
+COLUMN dbtime    FORMAT 999,999.99
+
+ALTER SESSION SET nls_date_format = 'yyyy-mm-dd hh24:mi:ss';
+
+WITH st AS
+(
+  SELECT snap_id
+       , dbid
+       , instance_number
+       , end_interval_time
+  FROM dba_hist_snapshot
+),
+stm AS
+(
+  SELECT snap_id
+       , dbid
+       , instance_number
+       , stat_name
+       , value
+  FROM dba_hist_sys_time_model
+  WHERE stat_name = 'DB time'
+),
+dbtime_per_hour AS
+(
+  SELECT CAST(st.end_interval_time AS DATE) snap_date_time                                                                                   -- the group column
+       , stm.stat_name                                                                                                                       -- the series column
+       , ROUND((stm.value - LAG(stm.value, 1, 0) OVER (PARTITION BY stm.dbid, stm.instance_number ORDER BY stm.snap_id))/1e6/6e1, 2) dbtime  -- the value column
+  FROM st
+     , stm
+  WHERE st.snap_id = stm.snap_id
+  AND   st.instance_number = stm.instance_number
+  AND   st.dbid = stm.dbid
+  AND   CAST(st.end_interval_time AS DATE) >= SYSDATE - 31
+),
+dbtime_per_day AS
+(
+  SELECT TO_CHAR(snap_date_time, 'yyyy-mm-dd') snap_date
+       , stat_name
+       , ROUND(SUM(dbtime)/COUNT(snap_date_time), 2) dbtime
+  FROM dbtime_per_hour
+  GROUP BY TO_CHAR(snap_date_time, 'yyyy-mm-dd')
+         , stat_name
+  ORDER BY snap_date
+)
+SELECT snap_date  -- the group column
+     , stat_name  -- the series column
+     , dbtime     -- the value column
+FROM dbtime_per_day
+WHERE dbtime NOT IN (SELECT MAX(dbtime) FROM dbtime_per_day)
+;
+
+-- DB time in Last 31 Days (interval by each hour).
+
+SET LINESIZE 200
+SET PAGESIZE 200
+
+COLUMN snap_date_time FORMAT a19
+COLUMN stat_name      FORMAT a10
+COLUMN dbtime         FORMAT 999,999.99
+
+ALTER SESSION SET nls_date_format = 'yyyy-mm-dd hh24:mi:ss';
+
+WITH st AS
+(
+  SELECT snap_id
+       , dbid
+       , instance_number
+       , end_interval_time
+  FROM dba_hist_snapshot
+),
+stm AS
+(
+  SELECT snap_id
+       , dbid
+       , instance_number
+       , stat_name
+       , value
+  FROM dba_hist_sys_time_model
+  WHERE stat_name = 'DB time'
+),
+dbtime_per_hour AS
+(
+  SELECT CAST(st.end_interval_time AS DATE) snap_date_time
+       , stm.stat_name
+       , ROUND((stm.value - LAG(stm.value, 1, 0) OVER (PARTITION BY stm.dbid, stm.instance_number ORDER BY stm.snap_id))/1e6/6e1, 2) dbtime
+  FROM st
+     , stm
+  WHERE st.snap_id = stm.snap_id
+  AND   st.instance_number = stm.instance_number
+  AND   st.dbid = stm.dbid
+  AND   CAST(st.end_interval_time AS DATE) >= SYSDATE - 31
+  ORDER BY snap_date_time
+)
+SELECT snap_date_time  -- the group column
+     , stat_name       -- the series column
+     , dbtime          -- the value column
+FROM dbtime_per_hour
+WHERE dbtime NOT IN (SELECT MAX(dbtime) FROM dbtime_per_hour)
+;
+
+-- DB time in Last 7 Days (interval by each day).
+
+SET LINESIZE 200
+SET PAGESIZE 200
+
+COLUMN snap_date FORMAT a10
+COLUMN stat_name FORMAT a10
+COLUMN dbtime    FORMAT 999,999.99
+
+ALTER SESSION SET nls_date_format = 'yyyy-mm-dd hh24:mi:ss';
+
+WITH st AS
+(
+  SELECT snap_id
+       , dbid
+       , instance_number
+       , end_interval_time
+  FROM dba_hist_snapshot
+),
+stm AS
+(
+  SELECT snap_id
+       , dbid
+       , instance_number
+       , stat_name
+       , value
+  FROM dba_hist_sys_time_model
+  WHERE stat_name = 'DB time'
+),
+dbtime_per_hour AS
+(
+  SELECT CAST(st.end_interval_time AS DATE) snap_date_time
+       , stm.stat_name
+       , ROUND((stm.value - LAG(stm.value, 1, 0) OVER (PARTITION BY stm.dbid, stm.instance_number ORDER BY stm.snap_id))/1e6/6e1, 2) dbtime
+  FROM st
+     , stm
+  WHERE st.snap_id = stm.snap_id
+  AND   st.instance_number = stm.instance_number
+  AND   st.dbid = stm.dbid
+  AND   CAST(st.end_interval_time AS DATE) >= SYSDATE - 7
+),
+dbtime_per_day AS
+(
+  SELECT TO_CHAR(snap_date_time, 'yyyy-mm-dd') snap_date
+       , stat_name
+       , ROUND(SUM(dbtime)/COUNT(snap_date_time), 2) dbtime
+  FROM dbtime_per_hour
+  GROUP BY TO_CHAR(snap_date_time, 'yyyy-mm-dd')
+         , stat_name
+  ORDER BY snap_date
+)
+SELECT snap_date  -- the group column
+     , stat_name  -- the series column
+     , dbtime     -- the value column
+FROM dbtime_per_day
+WHERE dbtime NOT IN (SELECT MAX(dbtime) FROM dbtime_per_day)
+;
+
+-- DB time in Last 7 Days (interval by each hour).
+
+SET LINESIZE 200
+SET PAGESIZE 200
+
+COLUMN snap_date_time FORMAT a19
+COLUMN stat_name      FORMAT a10
+COLUMN dbtime         FORMAT 999,999.99
+
+ALTER SESSION SET nls_date_format = 'yyyy-mm-dd hh24:mi:ss';
+
+WITH st AS
+(
+  SELECT snap_id
+       , dbid
+       , instance_number
+       , end_interval_time
+  FROM dba_hist_snapshot
+),
+stm AS
+(
+  SELECT snap_id
+       , dbid
+       , instance_number
+       , stat_name
+       , value
+  FROM dba_hist_sys_time_model
+  WHERE stat_name = 'DB time'
+),
+dbtime_per_hour AS
+(
+  SELECT CAST(st.end_interval_time AS DATE) snap_date_time
+       , stm.stat_name
+       , ROUND((stm.value - LAG(stm.value, 1, 0) OVER (PARTITION BY stm.dbid, stm.instance_number ORDER BY stm.snap_id))/1e6/6e1, 2) dbtime
+  FROM st
+     , stm
+  WHERE st.snap_id = stm.snap_id
+  AND   st.instance_number = stm.instance_number
+  AND   st.dbid = stm.dbid
+  AND   CAST(st.end_interval_time AS DATE) >= SYSDATE - 7
+  ORDER BY snap_date_time
+)
+SELECT snap_date_time  -- the group column
+     , stat_name       -- the series column
+     , dbtime          -- the value column
+FROM dbtime_per_hour
+WHERE dbtime NOT IN (SELECT MAX(dbtime) FROM dbtime_per_hour)
+;
+
+-- DB time in Last 24 Hours.
+
+SET LINESIZE 200
+SET PAGESIZE 200
+
+COLUMN snap_date_time FORMAT a19
+COLUMN stat_name      FORMAT a10
+COLUMN dbtime         FORMAT 999,999.99
+
+ALTER SESSION SET nls_date_format = 'yyyy-mm-dd hh24:mi:ss';
+
+WITH st AS
+(
+  SELECT snap_id
+       , dbid
+       , instance_number
+       , end_interval_time
+  FROM dba_hist_snapshot
+),
+stm AS
+(
+  SELECT snap_id
+       , dbid
+       , instance_number
+       , stat_name
+       , value
+  FROM dba_hist_sys_time_model
+  WHERE stat_name = 'DB time'
+),
+dbtime_per_hour AS
+(
+  SELECT CAST(st.end_interval_time AS DATE) snap_date_time
+       , stm.stat_name
+       , ROUND((stm.value - LAG(stm.value, 1, 0) OVER (PARTITION BY stm.dbid, stm.instance_number ORDER BY stm.snap_id))/1e6/6e1, 2) dbtime
+  FROM st
+     , stm
+  WHERE st.snap_id = stm.snap_id
+  AND   st.instance_number = stm.instance_number
+  AND   st.dbid = stm.dbid
+  AND   CAST(st.end_interval_time AS DATE) >= SYSDATE - INTERVAL '25' HOUR  -- http://blog.sina.com.cn/s/blog_6d18c1420102v1z7.html
+  ORDER BY snap_date_time
+)
+SELECT snap_date_time  -- the group column
+     , stat_name       -- the series column
+     , dbtime          -- the value column
+FROM dbtime_per_hour
+WHERE dbtime NOT IN (SELECT MAX(dbtime) FROM dbtime_per_hour)
+;
+
+-- DB time in Last 1 Hour.
+
+SET LINESIZE 200
+SET PAGESIZE 200
+
+COLUMN snap_date_time FORMAT a19
+COLUMN stat_name      FORMAT a10
+COLUMN dbtime         FORMAT 999,999.99
+
+ALTER SESSION SET nls_date_format = 'yyyy-mm-dd hh24:mi:ss';
+
+WITH st AS
+(
+  SELECT snap_id
+       , dbid
+       , instance_number
+       , end_interval_time
+  FROM dba_hist_snapshot
+),
+stm AS
+(
+  SELECT snap_id
+       , dbid
+       , instance_number
+       , stat_name
+       , value
+  FROM dba_hist_sys_time_model
+  WHERE stat_name = 'DB time'
+),
+dbtime_per_hour AS
+(
+  SELECT CAST(st.end_interval_time AS DATE) snap_date_time
+       , stm.stat_name
+       , ROUND((stm.value - LAG(stm.value, 1, 0) OVER (PARTITION BY stm.dbid, stm.instance_number ORDER BY stm.snap_id))/1e6/6e1, 2) dbtime
+  FROM st
+     , stm
+  WHERE st.snap_id = stm.snap_id
+  AND   st.instance_number = stm.instance_number
+  AND   st.dbid = stm.dbid
+  AND   CAST(st.end_interval_time AS DATE) >= SYSDATE - INTERVAL '2' HOUR  -- http://blog.sina.com.cn/s/blog_6d18c1420102v1z7.html
+  ORDER BY snap_date_time
+)
+SELECT snap_date_time
+     , stat_name
+     , dbtime
+FROM dbtime_per_hour
+WHERE dbtime NOT IN (SELECT MAX(dbtime) FROM dbtime_per_hour)
+;
+
+-- DB time Custom Time Period (interval by each day).
+
+SET LINESIZE 200
+SET PAGESIZE 200
+
+COLUMN snap_date FORMAT a10
+COLUMN stat_name FORMAT a10
+COLUMN dbtime    FORMAT 999,999.99
+
+ALTER SESSION SET nls_date_format = 'yyyy-mm-dd hh24:mi:ss';
+
+WITH st AS
+(
+  SELECT snap_id
+       , dbid
+       , instance_number
+       , end_interval_time
+  FROM dba_hist_snapshot
+),
+stm AS
+(
+  SELECT snap_id
+       , dbid
+       , instance_number
+       , stat_name
+       , value
+  FROM dba_hist_sys_time_model
+  WHERE stat_name = 'DB time'
+),
+dbtime_per_hour AS
+(
+  SELECT CAST(st.end_interval_time AS DATE) snap_date_time                                                                                   -- the group column
+       , stm.stat_name                                                                                                                       -- the series column
+       , ROUND((stm.value - LAG(stm.value, 1, 0) OVER (PARTITION BY stm.dbid, stm.instance_number ORDER BY stm.snap_id))/1e6/6e1, 2) dbtime  -- the value column
+  FROM st
+     , stm
+  WHERE st.snap_id = stm.snap_id
+  AND   st.instance_number = stm.instance_number
+  AND   st.dbid = stm.dbid
+  AND   (CAST(st.end_interval_time AS DATE) BETWEEN TO_DATE(:start_date, 'yyyy-mm-dd')
+                                            AND     TO_DATE(:end_date, 'yyyy-mm-dd')
+        )
+),
+dbtime_per_day AS
+(
+  SELECT TO_CHAR(snap_date_time, 'yyyy-mm-dd') snap_date
+       , stat_name
+       , ROUND(SUM(dbtime)/COUNT(snap_date_time), 2) dbtime
+  FROM dbtime_per_hour
+  GROUP BY TO_CHAR(snap_date_time, 'yyyy-mm-dd')
+         , stat_name
+  ORDER BY snap_date
+)
+SELECT snap_date  -- the group column
+     , stat_name  -- the series column
+     , dbtime     -- the value column
+FROM dbtime_per_day
+WHERE dbtime NOT IN (SELECT MAX(dbtime) FROM dbtime_per_day)
+;
+
+-- DB time Custom Time Period (interval by each hour).
+
+SET LINESIZE 200
+SET PAGESIZE 200
+
+COLUMN snap_date_time FORMAT a19
+COLUMN stat_name      FORMAT a10
+COLUMN dbtime         FORMAT 999,999.99
+
+ALTER SESSION SET nls_date_format = 'yyyy-mm-dd hh24:mi:ss';
+
+WITH st AS
+(
+  SELECT snap_id
+       , dbid
+       , instance_number
+       , end_interval_time
+  FROM dba_hist_snapshot
+),
+stm AS
+(
+  SELECT snap_id
+       , dbid
+       , instance_number
+       , stat_name
+       , value
+  FROM dba_hist_sys_time_model
+  WHERE stat_name = 'DB time'
+),
+dbtime_per_hour AS
+(
+  SELECT CAST(st.end_interval_time AS DATE) snap_date_time
+       , stm.stat_name
+       , ROUND((stm.value - LAG(stm.value, 1, 0) OVER (PARTITION BY stm.dbid, stm.instance_number ORDER BY stm.snap_id))/1e6/6e1, 2) dbtime
+  FROM st
+     , stm
+  WHERE st.snap_id = stm.snap_id
+  AND   st.instance_number = stm.instance_number
+  AND   st.dbid = stm.dbid
+  AND   (CAST(st.end_interval_time AS DATE) BETWEEN TO_DATE(:start_date, 'yyyy-mm-dd hh24:mi:ss')
+                                            AND     TO_DATE(:end_date, 'yyyy-mm-dd hh24:mi:ss')
+        )
+  ORDER BY snap_date_time
+)
+SELECT snap_date_time  -- the group column
+     , stat_name       -- the series column
+     , dbtime          -- the value column
+FROM dbtime_per_hour
+WHERE dbtime NOT IN (SELECT MAX(dbtime) FROM dbtime_per_hour)
+;
+
+-- The original code.
 
 SET LINESIZE 200
 SET PAGESIZE 200
