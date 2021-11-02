fe287c3886a3220c1475c429894fd169c79f12dd
Quanwen Zhao <guestart@163.com>
2021-11-02 11:14:47 +0800

    Update active_sessions_per_wait_class.sql

--- ash/active_sessions_per_wait_class.sql
+++ ash/active_sessions_per_wait_class.sql
@@ -3,6 +3,23 @@ REM     Script:        active_sessions_per_wait_class.sql
 REM     Author:        Quanwen Zhao
 REM     Dated:         Oct 29, 2021
 REM
+REM     Updated:       Nov 02, 2021
+REM                    (1) Replacing "NVL(wait_class, 'CPU') wait_class" with this, such as,
+REM                        "DECODE(wait_class, 'Administrative', 'Administrative'
+REM                                          , 'Application'   , 'Application'
+REM                                          , 'Cluster'       , 'Cluster'
+REM                                          , 'Commit'        , 'Commit'
+REM                                          , 'Concurrency'   , 'Concurrency'
+REM                                          , 'Configuration' , 'Configuration'
+REM                                          , 'Network'       , 'Network'
+REM                                          , 'Other'         , 'Other'
+REM                                          , 'Scheduler'     , 'Scheduler'
+REM                                          , 'System I/O'    , 'System I/O'
+REM                                          , 'User I/O'      , 'User I/O'
+REM                                          , NULL            , 'CPU'
+REM                               ) wait_class";
+REM                    (2) Adding extra 2 number of SQL snippets about "excluding/including BACKGROUND processes in Last 24 Hours";
+REM
 REM     Last tested:
 REM             11.2.0.4
 REM             19.3.0.0
@@ -12,7 +29,7 @@ REM     Purpose:
 REM       Visualizing the oracle active sessions per wait class in the view "v$active_session_history" in the last 1 hour by the custom report of SQL Developer.
 REM
 
--- Active Sessions Per Wait Class excluding BACKGROUND processes in Last 1 Hour:
+-- Active Sessions (in ASH) Per Wait Class excluding BACKGROUND processes in Last 1 Hour:
 
 SET LINESIZE 200
 SET PAGESIZE 200
@@ -26,7 +43,19 @@ WITH ash AS
 (
   SELECT TRUNC(sample_time, 'mi') sample_time
          -- TO_CHAR(CAST(sample_time AS DATE), 'yyyy-mm-dd hh24:mi') sample_time
-       , NVL(wait_class, 'CPU') wait_class
+       , DECODE(wait_class, 'Administrative', 'Administrative'
+                          , 'Application'   , 'Application'
+                          , 'Cluster'       , 'Cluster'
+                          , 'Commit'        , 'Commit'
+                          , 'Concurrency'   , 'Concurrency'
+                          , 'Configuration' , 'Configuration'
+                          , 'Network'       , 'Network'
+                          , 'Other'         , 'Other'
+                          , 'Scheduler'     , 'Scheduler'
+                          , 'System I/O'    , 'System I/O'
+                          , 'User I/O'      , 'User I/O'
+                          , NULL            , 'CPU'
+               ) wait_class
        , ROUND(COUNT(*)/6e1, 2) active_sessions
   FROM v$active_session_history
   WHERE session_type = 'FOREGROUND'  -- excluding background processes
@@ -41,7 +70,7 @@ SELECT * FROM ash
 WHERE sample_time >= SYSDATE - INTERVAL '60' MINUTE
 ;
 
--- Active Sessions Per Wait Class including BACKGROUND processes in Last 1 Hour:
+-- Active Sessions (in ASH) Per Wait Class including BACKGROUND processes in Last 1 Hour:
 
 SET LINESIZE 200
 SET PAGESIZE 200
@@ -55,7 +84,19 @@ WITH ash AS
 (
   SELECT TRUNC(sample_time, 'mi') sample_time
          -- TO_CHAR(CAST(sample_time AS DATE), 'yyyy-mm-dd hh24:mi') sample_time
-       , NVL(wait_class, 'CPU') wait_class
+       , DECODE(wait_class, 'Administrative', 'Administrative'
+                          , 'Application'   , 'Application'
+                          , 'Cluster'       , 'Cluster'
+                          , 'Commit'        , 'Commit'
+                          , 'Concurrency'   , 'Concurrency'
+                          , 'Configuration' , 'Configuration'
+                          , 'Network'       , 'Network'
+                          , 'Other'         , 'Other'
+                          , 'Scheduler'     , 'Scheduler'
+                          , 'System I/O'    , 'System I/O'
+                          , 'User I/O'      , 'User I/O'
+                          , NULL            , 'CPU'
+               ) wait_class
        , ROUND(COUNT(*)/6e1, 2) active_sessions
   FROM v$active_session_history
   WHERE session_type IN ('BACKGROUND', 'FOREGROUND')  -- including background processes
@@ -68,6 +109,88 @@ WITH ash AS
 )
 SELECT * FROM ash
 WHERE sample_time >= SYSDATE - INTERVAL '60' MINUTE
+;
+
+-- Active Sessions (in ASH) Per Wait Class excluding BACKGROUND processes in Last 24 Hours:
+
+SET LINESIZE 200
+SET PAGESIZE 200
+
+COLUMN sample_time FORMAT a19
+COLUMN wait_class  FORMAT a15
+
+ALTER SESSION SET nls_date_format = 'yyyy-mm-dd hh24:mi:ss';
+
+WITH ash AS
+(
+  SELECT TRUNC(sample_time, 'hh24') sample_time
+         -- TO_CHAR(CAST(sample_time AS DATE), 'yyyy-mm-dd hh24:mi') sample_time
+       , DECODE(wait_class, 'Administrative', 'Administrative'
+                          , 'Application'   , 'Application'
+                          , 'Cluster'       , 'Cluster'
+                          , 'Commit'        , 'Commit'
+                          , 'Concurrency'   , 'Concurrency'
+                          , 'Configuration' , 'Configuration'
+                          , 'Network'       , 'Network'
+                          , 'Other'         , 'Other'
+                          , 'Scheduler'     , 'Scheduler'
+                          , 'System I/O'    , 'System I/O'
+                          , 'User I/O'      , 'User I/O'
+                          , NULL            , 'CPU'
+               ) wait_class
+       , ROUND(COUNT(*)/36e2, 4) active_sessions
+  FROM v$active_session_history
+  WHERE session_type = 'FOREGROUND'  -- excluding background processes
+  AND   (wait_class <> 'Idle' OR wait_class IS NULL)
+  GROUP BY TRUNC(sample_time, 'hh24')
+           -- TO_CHAR(CAST(sample_time AS DATE), 'yyyy-mm-dd hh24:mi')
+         , wait_class
+  ORDER BY wait_class
+         , sample_time
+)
+SELECT * FROM ash
+WHERE sample_time >= SYSDATE - INTERVAL '24' HOUR
+;
+
+-- Active Sessions (in ASH) Per Wait Class including BACKGROUND processes in Last 24 Hours:
+
+SET LINESIZE 200
+SET PAGESIZE 200
+
+COLUMN sample_time FORMAT a19
+COLUMN wait_class  FORMAT a15
+
+ALTER SESSION SET nls_date_format = 'yyyy-mm-dd hh24:mi:ss';
+
+WITH ash AS
+(
+  SELECT TRUNC(sample_time, 'hh24') sample_time
+         -- TO_CHAR(CAST(sample_time AS DATE), 'yyyy-mm-dd hh24:mi') sample_time
+       , DECODE(wait_class, 'Administrative', 'Administrative'
+                          , 'Application'   , 'Application'
+                          , 'Cluster'       , 'Cluster'
+                          , 'Commit'        , 'Commit'
+                          , 'Concurrency'   , 'Concurrency'
+                          , 'Configuration' , 'Configuration'
+                          , 'Network'       , 'Network'
+                          , 'Other'         , 'Other'
+                          , 'Scheduler'     , 'Scheduler'
+                          , 'System I/O'    , 'System I/O'
+                          , 'User I/O'      , 'User I/O'
+                          , NULL            , 'CPU'
+               ) wait_class
+       , ROUND(COUNT(*)/36e2, 4) active_sessions
+  FROM v$active_session_history
+  WHERE session_type IN ('BACKGROUND', 'FOREGROUND')  -- including background processes
+  AND   (wait_class <> 'Idle' OR wait_class IS NULL)
+  GROUP BY TRUNC(sample_time, 'hh24')
+           -- TO_CHAR(CAST(sample_time AS DATE), 'yyyy-mm-dd hh24:mi')
+         , wait_class
+  ORDER BY wait_class
+         , sample_time
+)
+SELECT * FROM ash
+WHERE sample_time >= SYSDATE - INTERVAL '24' HOUR
 ;
 
 -- https://docs.oracle.com/cd/B19306_01/server.102/b14211/autostat.htm#CHDBCADD
