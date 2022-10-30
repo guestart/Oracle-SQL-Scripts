fd13b02ee2dc28afd1bb09bcef65ec41e7d07d1a
Quanwen Zhao <guestart@163.com>
2022-10-30 18:05:53 +0800

    Update poor_parsing_applications_rank_top_5_sql_on_sqlstats.sql

--- acquiring_pool_sql/poor_parsing_applications_rank_top_5_sql_on_sqlstats.sql
+++ acquiring_pool_sql/poor_parsing_applications_rank_top_5_sql_on_sqlstats.sql
@@ -2,6 +2,7 @@
 -- Script: poor_parsing_applications_rank_top_5_sql_on_sqlstats.sql
 -- Author: Quanwen Zhao
 -- Updated: May 17, 2019
+-- Modified: Oct 30, 2022 (modified v$sql to v$sqlstats)
 -- Ranking Top 5 SQL for poor parsing applications (parse_calls/executions) on v$sqlstats
 -- parse_calls, Number of parse calls for all cursors with this SQL text and plan
 -- executions, Number of executions that took place on this object since it was brought into the library cache
@@ -22,7 +23,7 @@ FROM (SELECT sql_id
              , sql_text
              , parse_calls/executions
              , DENSE_RANK() OVER (ORDER BY DECODE(executions, 0, 0, parse_calls / executions) DESC) AS parse_app_rank
-      FROM v$sql
+      FROM v$sqlstats
       WHERE DECODE(executions, 0, 0, parse_calls / executions) > 1
      )
 WHERE parse_app_rank <= 5
