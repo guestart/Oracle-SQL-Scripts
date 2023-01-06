41189d0995eeabc6c1660f6a1d56c0a10dcb8999
Quanwen Zhao <guestart@163.com>
2023-01-06 22:16:48 +0800

    Create rman_backup_job_details.sql

---
+++ ShinData/rman_backup_job_details.sql
@@ -0,0 +1,25 @@
+REM
+REM     Script:        rman_backup_job_details.sql
+REM     Author:        Quanwen Zhao
+REM     Dated:         DEC 03, 2022
+REM
+REM     Last tested:
+REM             11.2.0.4
+REM             12.2.0.1
+REM             19.3.0.0
+REM
+REM     Purpose:
+REM       Checking the rman backup job details on your oracle database.
+REM
+
+select input_type,
+       status,
+       to_char(start_time,'yyyy-mm-dd hh24:mi:ss') start_time,
+       to_char(end_time, 'yyyy-mm-dd hh24:mi:ss') end_time,
+       time_taken_display,
+       input_bytes_display,
+       output_bytes_display,
+       input_bytes_per_sec_display,
+       output_bytes_per_sec_display
+from v$rman_backup_job_details
+order by 3 desc;
