9a8cf05b8c6314a5f175b6ee7a81be19047b1e68
Quanwen Zhao <guestart@163.com>
2023-01-08 15:47:07 +0800

    Create all_params.sql

---
+++ ShinData/all_params.sql
@@ -0,0 +1,20 @@
+REM
+REM     Script:        all_params.sql
+REM     Author:        Quanwen Zhao
+REM     Dated:         Aug 01, 2022
+REM
+REM     Last tested:
+REM             11.2.0.4
+REM             12.2.0.1
+REM             19.3.0.0
+REM
+REM     Purpose:
+REM       Checking all of the parameters of oracle database.
+REM
+
+select name,
+       decode(type, 1, 'Boolean', 2, 'String', 3, 'Integer', 4, 'Parameter file', 5, 'Reserved', 6, 'Big integer') type,
+       value,
+       description
+from v$parameter 
+order by name;
